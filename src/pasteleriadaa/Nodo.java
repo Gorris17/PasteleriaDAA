099e29b1ddb63d8ef36212ac6ab94f990aa491cf
hola <hola@10.0.88.60>
2016-04-14 20:00:19 +0200

    Merge origin/master
    Conflicts:
    	src/pasteleriadaa/PasteleriaDAA.java

---
+++ src/pasteleriadaa/Nodo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pasteleriadaa;
+
+/**
+ *
+ * @author hola
+ */
+public class Nodo implements Comparable <Nodo>{
+
+    @Override
+    public int compareTo(Nodo t) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
